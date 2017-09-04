811ce57a5c3902c85b1913a26a23b12c06d9453a
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-09-04 18:03:51 +0200

    router basic and delgated

--- 2-spa/cash-flow/src/app/app.module.ts
+++ 2-spa/cash-flow/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from "./tools/components/components.module";
 import { AppRoutingModule } from "./app.routing";
+import { HomeModule } from "./views/home/home.module";
+import { AboutModule } from "./views/about/about.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AboutModule,
     AppRoutingModule,
     BrowserModule,
     ComponentsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 2-spa/cash-flow/src/app/tools/components/components.module.ts
+++ 2-spa/cash-flow/src/app/tools/components/components.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './shell/top-bar/top-bar.component';
 import { MainContentComponent } from './shell/main-content/main-content.component';
 import { ShellComponent } from './shell/shell.component';
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [TopBarComponent, MainContentComponent, ShellComponent],
   exports: [ShellComponent]
